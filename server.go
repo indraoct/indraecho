9982f32cc5be0c8b7956b4add5111ea8ea0a3d6c
Indra Octama <omyank2007i@gmail.com>
2017-04-16 01:11:13 +0700

    second commit

--- server.go
+++ server.go
@@ -3,9 +3,7 @@ package main
 
 import (
 	"net/http"
-	"math/rand"
 	"github.com/labstack/echo"
-	"time"
 	"indraecho/models"
 )
 
@@ -16,29 +14,19 @@ func main() {
         	return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	// JSONP
-	e.GET("/jsonp", func(c echo.Context) error {
-		var content struct {
-			Response  string    `json:"response"`
-			Timestamp time.Time `json:"timestamp"`
-			Random    int       `json:"random"`
-		}
-		content.Response = "Sent via JSONP"
-		content.Timestamp = time.Now().UTC()
-		content.Random = rand.Intn(1000)
-		return c.JSON(1,content)
-	})
-
-	//JSONUser
+	/**
+	 * API JSON USER
+	 */
 	e.GET("jsonuser", func(c echo.Context) error{
 		var content struct{
 			Status string  `json:"status"`
+			Msg string `json:"msg"`
 			Data []map[string]interface{}  `json:"data"`
 		}
 
 		content.Status = "1"
-                var data,_  = models.GetuserList()
-		content.Data = data
+		content.Msg = "Success"
+		content.Data,_ = models.GetuserList()
 
 		return c.JSON(1,&content)
 	})
